02d655c00e7d52ca8d9c629013c21ff9d8265a30
William Han <William.Han2@Honeywell.com>
2017-06-02 23:57:58 -0400

    added all files

---
+++ app.py
@@ -0,0 +1,68 @@
+import xlrd
+
+from flask import Flask, render_template, request, redirect
+import mailing
+
+app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = set(['csv', 'xlsx', 'xls'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def parse_upload_form():
+    f = request.files['file']
+    data = []
+    if f and allowed_file(f.filename):
+        extension = str(f.filename.rsplit('.', 1)[1].lower())
+        if extension == "xlsx" or extension == "xls":
+            workbook = xlrd.open_workbook(file_contents=f.read())
+            worksheet = workbook.sheet_by_index(0)
+            data = list(cast_row(worksheet.row_values(rx, 0, 4)) for rx in range(worksheet.nrows) if worksheet.row_len(rx) == 4)
+        elif extension == "csv":
+            data = data_from_csv_string(f.read().decode("utf-8"))
+    else:
+        csv = request.form['data']
+        data = data_from_csv_string(csv)
+    return data
+
+
+@app.route('/item', methods=['POST'])
+def item():
+    data = parse_upload_form()
+    print(data);
+    return(render_template('sent.html'))
+
+@app.route('/')
+def renderPage():
+    return(render_template('index.html'))
+
+def cast_row(row):
+    '''
+    Convert workbook sheet cells into integers if they are equal to integer
+    values and convert everything to a string.
+    The xlrd library seems to import cells as float values if the cell had a
+    numeric value, so this method is needed to correct that.
+    '''
+    for i, item in enumerate(row):
+        if isinstance(item, (float, int)) and int(item) == item:
+            row[i] = str(int(item))
+        else:
+            row[i] = str(item)
+    return row
+
+
+def data_to_csv_string(data):
+    output = io.StringIO()
+    writer = csv.writer(output)
+    writer.writerows(data)
+    return output.getvalue()
+
+def data_from_csv_string(string):
+    data_input = io.StringIO(string)
+    reader = csv.reader(data_input)
+    return list(reader)
+
+
+app.run(debug=True)
---
+++ mailing.py
@@ -0,0 +1,29 @@
+# Sending function
+import smtplib
+
+# Email module
+from email.mime.text import MIMEText
+
+# These work, enjoy your new gmail account
+donut = "willycheestick6@gmail.com"
+pwd = "raymondchee"
+
+def sendEmail(txt, to):
+
+	print("Sending email...\n")
+	smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+	smtpserver.ehlo()
+	smtpserver.starttls()
+	smtpserver.ehlo
+	smtpserver.login(donut, pwd)
+
+	header = "To: " + to + "\n" + "From: " + donut
+	header = header + "\n" + "Subject: Donut Reply :-)\n"
+
+	msg = header + "\n" + txt + "\n\n"
+	smtpserver.sendmail(donut, to, msg)
+	smtpserver.close()
+
+	return
+
+#sendEmail("hi", copperstick6@gmail.com)
